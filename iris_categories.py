c590f5a0cf93bb7b269cea389ed5b6c8a8fe3715
Connor Mackay <connor.mackay@gmail.com>
2023-07-06 17:14:06 -0400

    implement logistic regression model

--- iris_categories.py
+++ iris_categories.py
@@ -44,3 +44,11 @@ y= array[:, 4]
 X_train, X_validation, Y_train, Y_validation = train_test_split(x, y, test_size=0.20, random_state=1)
 
 
+## Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+
+results = []
+names = []
+# for name, model in models:
+    
