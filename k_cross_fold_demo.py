d4230dc707ee685175d7adec1099f7b518436398
Connor Mackay <connor.mackay@gmail.com>
2023-07-06 16:45:07 -0400

    implement demo of Kfold method

--- k_cross_fold_demo.py
+++ k_cross_fold_demo.py
@@ -1,1 +1,10 @@
-dataset = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+from numpy import array
+from sklearn.model_selection import KFold
+
+dataset = array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
+
+kfold = KFold(n_splits=3, shuffle=True, random_state=1)
+
+for train, test in kfold.split(dataset):
+    print('train, %s, test, %s' % (dataset[train], dataset[test]))
+
