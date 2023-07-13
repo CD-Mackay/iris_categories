4715a303a2f57f70e5a270fbd8192be1ec88288b
Connor Mackay <connor.mackay@gmail.com>
2023-07-13 13:34:23 -0400

    define plot loss curve method

--- linear_regression_tutorial.py
+++ linear_regression_tutorial.py
@@ -57,4 +57,13 @@ def plot_the_model(trained_weight, trained_bias, feature, label):
     plt.plot([x0, x1], [y0, y1], c='r')
     plt.show()
 
+def plot_the_loss_curve(epochs, rmse):
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Root Mean Squared Error')
 
+    plt.plot(epochs, rmse, label='Loss')
+    plt.legend()
+    plt.ylim([rmse.min()*0.97, rmse.max()])
+    plt.show()
+    
