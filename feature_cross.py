2ce4dcbcb8553e507ff7e9f8da1ae8583c7b0c8c
Connor Mackay <connor.mackay@gmail.com>
2023-07-18 11:30:54 -0400

    define plot loss curve method

--- feature_cross.py
+++ feature_cross.py
@@ -50,3 +50,11 @@ def train_model(model, dataset, epochs, batch_size, label_name):
     
 
 def plot_the_loss_curve():
+  plt.figure()
+  plt.xlabel("Epoch")
+  plt.ylabel("Root Mean Squared Error")
+
+  plt.plot(epochs, rmse, label="Loss")
+  plt.legend()
+  plt.ylim([rmse.min()*0.94, rmse.max()* 1.05])
+  plt.show()  
