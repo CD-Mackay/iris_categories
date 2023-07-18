25fc29637bb5548dfaf6648f089c4496fc509d53
Connor Mackay <connor.mackay@gmail.com>
2023-07-18 11:09:45 -0400

    load, scale and shuffle datasets

--- feature_cross.py
+++ feature_cross.py
@@ -8,3 +8,13 @@ pd.options.display.max_rows = 10
 pd.options.display.float_format = "{:.1f}".format
 
 tf.keras.backend.set_floatx('float32')
+
+train_df = pd.read_csv('california_housing_train.csv')
+test_df = pd.read_csv('california_housing_test.csv')
+
+scale_factor = 1000.0
+
+train_df['median_house_value'] /= scale_factor
+test_df['median_house_value'] /= scale_factor
+
+train_df = train_df.reindex(np.random.permutation(train_df.index))
