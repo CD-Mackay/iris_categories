0d4184b558a3632fea534be954ee06107fc497f3
Connor Mackay <connor.mackay@gmail.com>
2023-07-18 11:07:31 -0400

    import modules

---
+++ feature_cross.py
@@ -0,0 +1,10 @@
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+from tensorflow.keras import layers
+from matplotlib import pyplot as plt
+
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
+tf.keras.backend.set_floatx('float32')
