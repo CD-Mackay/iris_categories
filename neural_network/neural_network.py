abfb288acb79263bb6b36b0c1a4fe8bcf876218f
Connor Mackay <connor.mackay@gmail.com>
2023-07-26 11:36:15 -0400

    implement normalization layer fr median income

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -11,3 +11,19 @@ train_df = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/californ
 train_df = train_df.reindex(np.random.permutation(train_df.index)) # shuffle the examples
 test_df = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_test.csv")
 
+inputs = {
+    'latitude':
+    tf.keras.layers.Input(shape=(1,), dtype=tf.float32, name='latitude'),
+    'longitude':
+    tf.keras.layers.Input(shape=(1,), dtype=tf.float32, name='longitude'),
+    'population': 
+    tf.keras.layers.Input(shape=(1,), dtype=tf.float32, name='population')
+}
+
+## Create normalization layer for median income
+median_income = tf.keras.layers.Normalization(
+    name='normalization_median_income',
+    axis=None
+)
+
+median_income.adapt(train_df['median_income'])
