272c6d83f9a17f93e70a15ca0f21d8cbbb5ea219
Connor Mackay <connor.mackay@gmail.com>
2023-07-27 18:33:59 -0400

    define plot the loss curve method

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -75,3 +75,22 @@ feature_cross = tf.keras.layers.HashedCrssing(
 preprocessing_layers = tf.keras.layers.Concatenate()(
     [feature_cross, median_income, population]
 )
+
+def plot_the_loss_curve(epochs, mse_training, mse_validation):
+    plt.figure()
+    plt.xlabel("Epoch")
+    plt.ylabel("Mean Squared Error")
+
+    plt.plot(epochs, mse_training, label="Training Loss")
+    plt.plot(epochs, mse_validation, label="Validation Loss")
+
+    merged_mse_lists = mse_training.tolist() + mse_validation
+    highest_loss = max(merged_mse_lists)
+    lowest_loss = min(merged_mse_lists)
+    top_of_y_axis = highest_loss * 1.03
+    bottom_of_y_axis = lowest_loss * 0.97
+
+    plt.ylim([bottom_of_y_axis, top_of_y_axis])
+    plt.legend()
+    plt.show()
+
